e66502d650bdc8cb85c9fc73f8b13840205b56fd
Wako <wakokunu@gmail.com>
2024-12-20 04:45:29 +0300

    Automated update of chess players data

--- chess_dashboard.py
+++ chess_dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import time
-import sqlite3
 
 # Custom CSS
 st.markdown(
@@ -696,3 +695,5 @@ with tab3:
 # Auto-refresh triggered at 2024-12-18T10:25:15.123585
 
 # Auto-refresh triggered at 2024-12-19T09:15:03.063216
+
+# Auto-refresh triggered at 2024-12-20T04:45:28.775464
